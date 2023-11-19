1a39ea439b6e8827617173b7da20d836b23a28c3
Minodeveloper <minoshideveloper8@gmail.com>
2023-11-19 10:53:03 +0530

    progressed

--- deitel/chapter_02/practise/practise/practise.cpp
+++ deitel/chapter_02/practise/practise/practise.cpp
@@ -2,11 +2,37 @@
 
 #include <iostream>
 
+//add integers 
+
+int main() {
+
+	int number1{ 0 };
+	
+	int number2{ 0 };
+
+	int sum{ 0 };
+
+	std::cout << "Enter first numner: ";
+	std::cin >> number1;
+
+	std::cout << "Enter second number: ";
+	std::cin >> number2;
+
+	sum = number1 + number2;
+
+	std::cout << "Sum is  " << sum << std::endl;
+
+	return 0;
+}
+
+
+//display message
+/*
 int main()
 {
 	std::cout << "Welcome to\n c ++" << std::endl;
 
-
-
 	return 0;
 }
+*/
+
