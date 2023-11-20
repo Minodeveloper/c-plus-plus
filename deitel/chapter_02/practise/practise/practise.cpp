24e49d5f6fafc5ccd416fbffa213d7c5424f9be2
Minodeveloper <minoshideveloper8@gmail.com>
2023-11-20 11:55:32 +0530

    understood assembly of if statements

--- deitel/chapter_02/practise/practise/practise.cpp
+++ deitel/chapter_02/practise/practise/practise.cpp
@@ -2,8 +2,36 @@
 
 #include <iostream>
 
-//add integers 
+//comparing two numbers
 
+int main() {
+
+	int number1{ 0 };
+	int number2{ 0 };
+
+	std::cout << "__COMPARING TWO NUMBERS__" << std::endl;
+
+	std::cout << "Enter two numbers: ";
+	std::cin >> number1 >> number2;
+
+	if (number1 == number2) {
+		std::cout << number1 << " == " << number2;
+	}
+	if (number1 != number2)
+	{
+		std::cout << number1 << " != " << number2;
+	}
+	if (number1 < number2) {
+		std::cout << number1 << " < " << number2;
+	}
+
+
+
+	return 0;
+}
+
+//add integers 
+/*
 int main() {
 
 	int number1{ 0 };
@@ -24,7 +52,7 @@ int main() {
 
 	return 0;
 }
-
+*/
 
 //display message
 
